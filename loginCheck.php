bd1e6a9e43c6c88b8fc697ec7bd698ee6b0c9a69
mythzaizai <mythzaizai@gmail.com>
2019-10-03 18:18:17 +0800

    Update loginCheck.php

--- loginCheck.php
+++ loginCheck.php
@@ -6,8 +6,10 @@
     ini_set("display_errors", "On");
     require_once "./connect.php";
 
-    $account = $_POST['account'];       //$account=0=0 ||
+    $account = $_POST['account'];       //$account=0=0
     $password = $_POST['password'];
+
+    
 /*
     if($connect -> prepare("SELECT * FROM employee WHERE 1")){
         echo "true";
@@ -15,11 +17,13 @@
         echo "false";
     }
 */
+
+/*
     $select = $connect -> prepare("SELECT employee_cellphone,employee_password FROM employee WHERE employee_cellphone = '$account' AND employee_password = '$password' ");
     $select -> execute(array('employee_cellphone' => $account,'employee_password' => $password));
     $result = $select -> fetch(PDO::FETCH_ASSOC);      //返回以欄位名稱作為key的陣列
     
-
+    
     if($account==$result['employee_cellphone'] &&  $password==$result['employee_password']){   //先暫時預設
         session_start();
         $_SESSION['member'] = $result;
@@ -29,6 +33,35 @@
     }else{
         header("Location:./?hint=帳號或密碼錯誤~");
     }
+    
+*/ 
+
+    $query = "SELECT * FROM employee WHERE employee_cellphone = '$account' AND employee_password = '$password' ";
+
+    mysqli_query("set names utf8");         //注意mysql 和 mysqli
+    $result=mysqli_query($connect,$query);
+    $rows = mysqli_num_rows($result);
+
+
+    if($rows == 1){      
+        
+        session_start();
+        $_SESSION['member'] = $result;
+        $_SESSION["is_login"] = "true";
+        header("Location:./?hint=歡迎!");  
+        
+    }else if($account == '' || $password == ''){
+        
+        header("Location:./?hint=輸入不完全~");   
+        
+    }else{
+        
+        header("Location:./?hint=帳號或密碼錯誤~"); 
+        
+    }
+    
+
+    
 
 
 ?>
